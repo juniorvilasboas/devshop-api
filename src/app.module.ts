0dc15055508186cf5e63755d4a76f59cc2dcb61b
Moacyr Santana <junior.vilasboas@gmail.com>
2022-02-01 16:36:17 +0000

    Config TypeORM with Postgre

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { GraphQLModule } from '@nestjs/graphql'
 import { CategoryModule } from './category/category.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.URL_POSTGRES,
+      autoLoadEntities: true,
+      synchronize: true
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql'
     }),
