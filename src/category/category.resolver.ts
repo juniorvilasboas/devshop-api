1fe5affbafafd2e6fdb94d8c6fe21be4e75586a9
Moacyr Santana <junior.vilasboas@gmail.com>
2022-08-27 10:45:04 +0100

    Fix category

--- src/category/category.resolver.ts
+++ src/category/category.resolver.ts
@@ -19,21 +19,21 @@ export class CategoryResolver {
     return await this.categoryService.findById(id)
   }
 
-  @Mutation(returns => CategoryPublic, { name: 'panelCreateCategory' })
+  @Mutation(returns => CategoryPublic, { name: 'createCategory' })
   async createCategory(
     @Args('input') input: CategoryCreateInput
   ): Promise<CategoryPublic> {
     return this.categoryService.create(CategoryMapper.toEntity(input))
   }
 
-  @Mutation(returns => CategoryPublic, { name: 'panelUpdateCategory' })
+  @Mutation(returns => CategoryPublic, { name: 'updateCategory' })
   async updateCategory(
     @Args('input') input: CategoryUpdateInput
   ): Promise<CategoryPublic> {
     return this.categoryService.update(input)
   }
 
-  @Mutation(returns => Boolean, { name: 'panelDeleteCategory' })
+  @Mutation(returns => Boolean, { name: 'deleteCategory' })
   async deleteCategory(@Args('id') input: string): Promise<boolean> {
     return this.categoryService.delete(input)
   }
