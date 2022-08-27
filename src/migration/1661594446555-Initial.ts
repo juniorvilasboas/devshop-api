b1ed05770a1cf320a88fddaeeede69d6c7340bfe
Moacyr Santana <junior.vilasboas@gmail.com>
2022-08-27 11:22:54 +0100

    Add typeorm migration

---
+++ src/migration/1661594446555-Initial.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class Initial1661594446555 implements MigrationInterface {
+  name = 'Initial1661594446555'
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "category" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(250) NOT NULL, "slug" character varying(250) NOT NULL, CONSTRAINT "PK_9c4e4a89e3674fc9f382d733f03" PRIMARY KEY ("id"))`
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "category"`)
+  }
+}
